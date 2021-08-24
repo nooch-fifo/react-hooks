beb790d2f174d018f7eca22e45b8e43da48ef384
Rego <943648@cognizant.com>
2021-08-24 14:35:43 -0400

    initial React project files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myName = "Dominic";
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          { myName } Learn React
         </a>
       </header>
     </div>
